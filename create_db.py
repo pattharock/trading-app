f1ef9b7f341a7ccae08d70cf9a5f696d76c4745e
pattharock <ritvik1609#gmail.com>
2021-02-16 10:23:27 +0800

    changed create_db.py

--- create_db.py
+++ create_db.py
@@ -11,18 +11,18 @@ cursor.execute("""
         name TEXT NOT NULL
     )
 """)
-cursor.execute("""
-    CREATE TABLE IF NOT EXISTS stock_price (
-        id INTEGER PRIMARY KEY, 
-        stock_id INTEGER,
-        date NOT NULL,
-        open NOT NULL, 
-        high NOT NULL, 
-        low NOT NULL, 
-        close NOT NULL, 
-        volume NOT NULL,
-        FOREIGN KEY (stock_id) REFERENCES stock (id)
-    )
-""")
+# cursor.execute("""
+#     CREATE TABLE IF NOT EXISTS stock_price (
+#         id INTEGER PRIMARY KEY, 
+#         stock_id INTEGER,
+#         date NOT NULL,
+#         open NOT NULL, 
+#         high NOT NULL, 
+#         low NOT NULL, 
+#         close NOT NULL, 
+#         volume NOT NULL,
+#         FOREIGN KEY (stock_id) REFERENCES stock (id)
+#     )
+# """)
 
 connection.commit()
